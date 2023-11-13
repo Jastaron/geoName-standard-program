38b3f2f20ec44f5851a557cc23a30e1059941658
Jastaron <101964727+Jastaron@users.noreply.github.com>
2023-11-13 14:31:03 +0800

    Delete WeiGeoNormalize directory

--- WeiGeoNormalize/Codes/SGNS.py
+++
@@ -1,8 +0,0 @@
-from Controller import *
-
-if __name__ == '__main__':
-    # Create the application instance
-    app = QApplication([])
-    controller = SGNS_Controller()
-    controller.show()
-    app.exec_()
